269ba43cca1b84d55dc0fb59a6914b92f0337064
sunil <sunildutt6@hotmail.com>
2020-10-25 19:19:52 +0100

    rectified

--- js/question-2.js
+++ js/question-2.js
@@ -1,7 +1,15 @@
+// Question 2
+// Make a call to the following API endpoint:
+// https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating
+// Loop through the results and display the following properties in HTML, but only for the first eight results:
+//     name
+//     rating
+//     number of tags (not the tag details, just the amount of tags)
+
+
 const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
 
 const resultClass = document.querySelector(".results");
-
 
 fetch(url)
     .then(promise => promise.json())
@@ -31,22 +39,20 @@ function createHtml(response) {
 
 
 
+//Solution 2
 
-
+// const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
+// const resultClass = document.querySelector(".results");
 
 // async function getResult() {
 
 //     try {
 //         const promise = await fetch(url);
-
 //         const response = await promise.json();
-
 //         const data = response.results;
-
 //         resultClass.innerHTML = "";
 
 //         for (let i = 0; i < data.length; i++) {
-
 //             if (i === 8) {
 //                 break;
 //             }
@@ -58,8 +64,6 @@ function createHtml(response) {
 //         }
 //     } catch (error) {
 //         resultClass.innerHTML = displayError("Error Occured there!!!")
-        
 //     }
-
 // }
 //   getResult();
